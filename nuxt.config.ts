1a93d72fed2c195a11dad04474692f20fb272718
Ion Petrascu <ionpetrascuc0318@gmail.com>
2024-06-05 16:36:56 +0300

    feat: load exeternal stylesheets

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: true },
-  ssr:true,
-  css:['~/assets/css/custom.css']
+  ssr: true,
+  css: ['~/assets/css/custom.css'],
+  app: {
+    head:{
+      link:[
+        {
+          rel:'stylesheet',
+          href:'https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css'
+        }
+      ]
+    }
+  }
+
 })
