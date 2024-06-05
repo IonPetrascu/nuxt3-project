a7ab2fe2eb065d5d7810b2e8e5805195bc5e6ccc
Ion Petrascu <ionpetrascuc0318@gmail.com>
2024-06-05 15:49:18 +0300

    feat: create pages

---
+++ pages/blog/[id].vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+const {params} = useRoute()
+const id = params.id
+
+
+</script>
+<template>
+<div>
+  single post page {{  id}}
+</div>
+</template>
+<style scoped>
+
+</style>
