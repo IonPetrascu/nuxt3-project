a529e8531449eed7a2b95edea877dc17173ae481
Ion Petrascu <ionpetrascuc0318@gmail.com>
2024-06-06 15:05:17 +0300

    feat: display data in home page

--- components/Post.vue
+++ components/Post.vue
@@ -1,15 +1,20 @@
 <script lang="ts" setup>
-
+  const props = defineProps({
+    post:{
+      type:Object,
+      require:true
+    }
+  })
 </script>
 <template>
   <article class="card mb-7 mt-1">
     <h3 class="text-xl mb-2">
-      <nuxt-link to="#"> Post Title</nuxt-link>
+      <nuxt-link :to="`/blog/${post.id}`">{{ post.title }}</nuxt-link>
     </h3>
-    <p class="text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, ipsum.</p>
-    <span class="block text-sm mt-2 mb-2">Created by: John Doe</span>
-    <time class="block text-sm text-blue-500"><span class="fa fa-clock"></span> 2023-10-01</time>
-    <NuxtLink to="#">Read more</NuxtLink>
+    <p class="text-sm">{{ post.description.substring(0, 100) }}...</p>
+    <span class="block text-sm mt-2 mb-2">Created by: {{ post.author }}</span>
+    <time class="block text-sm text-blue-500"><span class="fa fa-clock"></span> {{ post.time }}</time>
+    <NuxtLink :to="`/blog/${post.id}`">Read more</NuxtLink>
   </article>
 </template>
 <style scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,25 +1,15 @@
 <script lang="ts" setup>
-  const config = useRuntimeConfig()
-  console.log(config.public.apiBase);
+const config = useRuntimeConfig()
 
-
-  if(process.server){
-    console.log(config.secretKey);
-
-  }
+const apiBase = config.public.apiBase
+const { data: posts, pending, error } = await useFetch(`${apiBase}/posts`)
 </script>
 <template>
-<div >
-  <h1 class="text-3xl mb-4">Latest Posts</h1>
-  <section>
-    <Post/>
-    <Post/>
-    <Post/>
-    <Post/>
-    <Post/>
-  </section>
-</div>
+  <div>
+    <h1 class="text-3xl mb-4">Latest Posts</h1>
+    <section>
+      <Post :key="post.id" v-for="post in posts" :post="post" />
+    </section>
+  </div>
 </template>
-<style scoped>
-
-</style>
+<style scoped></style>
