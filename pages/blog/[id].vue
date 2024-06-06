e02bc4c014af32ea5db9d582a9b218836f6e0a8d
Ion Petrascu <ionpetrascuc0318@gmail.com>
2024-06-06 15:37:05 +0300

    feat: Displaying Single Post Data useAsyncData

--- pages/blog/[id].vue
+++ pages/blog/[id].vue
@@ -1,19 +1,26 @@
 <script lang="ts" setup>
-const {params} = useRoute()
-const id = params.id
+const config = useRuntimeConfig()
+const { params } = useRoute()
 
+const apiBase = config.public.apiBase
 
+//id=1  posts_p1,id=2 => post_p2
+const { data: post, pending, error } = await useAsyncData(`posts_p${params.id}`, () => $fetch(`${apiBase}/posts/${params.id}`))
 </script>
 <template>
-<div>
-  <h3 >
-      <nuxt-link class="text-xl text-gray-500 no-underline mb-2 text-center" to="#"> Post Title {{  id}}</nuxt-link>
+  <div v-if="pending">
+    Loading
+  </div>
+  <div v-else-if="error">
+    Error
+  </div>
+  <div v-else>
+    <h3>
+      <nuxt-link class="text-xl text-gray-500 no-underline mb-2 text-center" to="#">{{ post.title }}</nuxt-link>
     </h3>
-    <p class="text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, ipsum.</p>
-    <span class="block text-sm mt-2 mb-2">Created by: John Doe</span>
-    <time class="block text-sm text-blue-500"><span class="fa fa-clock"></span> 2023-10-01</time>
-</div>
+    <p class="text-sm">{{ post.description }}</p>
+    <span class="block text-sm mt-2 mb-2">Created by: {{ post.author }} </span>
+    <time class="block text-sm text-blue-500"><span class="fa fa-clock"></span>{{ post.time }}</time>
+  </div>
 </template>
-<style scoped>
-
-</style>
+<style scoped></style>
