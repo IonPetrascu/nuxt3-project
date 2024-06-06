af00afebebcf3e4e7e7ecb22e1896027b1899729
Ion Petrascu <ionpetrascuc0318@gmail.com>
2024-06-06 13:46:05 +0300

    feat: add post compoennt

---
+++ components/Post.vue
@@ -0,0 +1,15 @@
+<script lang="ts" setup>
+
+</script>
+<template>
+  <article class="card mb-7 mt-1">
+    <h3 class="text-xl mb-2">
+      <nuxt-link to="#"> Post Title</nuxt-link>
+    </h3>
+    <p class="text-sm">Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti, ipsum.</p>
+    <span class="block text-sm mt-2 mb-2">Created by: John Doe</span>
+    <time class="block text-sm text-blue-500">2023-10-01</time>
+    <NuxtLink to="#">Read more</NuxtLink>
+  </article>
+</template>
+<style scoped></style>
--- pages/index.vue
+++ pages/index.vue
@@ -2,8 +2,15 @@
 
 </script>
 <template>
-<div>
-  Home
+<div >
+  <h1 class="text-3xl mb-4">Latest Posts</h1>
+  <section>
+    <Post/>
+    <Post/>
+    <Post/>
+    <Post/>
+    <Post/>
+  </section>
 </div>
 </template>
 <style scoped>
